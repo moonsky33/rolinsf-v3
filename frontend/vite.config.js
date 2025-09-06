157853f712538833d60fa1f73944d66eecbe33fd
moonsky33 <3080693085@qq.com>
2025-09-06 20:27:56 +0800

    fix:修复vite配置

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: import.meta.env.MODE === 'production' 
-    ? '/rolinsf-v3/'  // 生产环境：必须与 GitHub 仓库名称一致
-    : '/' 
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [vue()],
+    base: mode === 'production' 
+      ? '/rolinsf-v3/'
+      : '/'
+  }
 })
